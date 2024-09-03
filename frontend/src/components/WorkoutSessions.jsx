9bed4db87ba34bbb7388deac7e92653b026ddbc1
Ansh-Majumdar619 <anshmajumdar619@gmail.com>
2024-09-03 21:50:03 +0530

    Initial commit with backend and frontend

---
+++ frontend/src/components/WorkoutSessions.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const WorkoutSessions = () => {
+  return (
+    <section className="workout_session">
+      <div className="wrapper">
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>
+        At Elite Edge, our top workout is designed to push your limits and maximize your potential. Combining high-intensity interval training (HIIT) with strength and conditioning exercises, this regimen is tailored to build muscle, burn fat, and enhance overall fitness. Each session is carefully crafted to challenge different muscle groups, ensuring a balanced workout that promotes endurance, power, and flexibility. Whether you're a seasoned athlete or just starting your fitness journey, our top workout at Elite Edge will help you achieve your goals and unlock your full athletic potential.
+        </p>
+        <img src="/img5.jpg" alt="workout" />
+      </div>
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMPS</h1>
+        <p>
+        "Transform Your Fitness: Discover Our Elite Bootcamps"
+        </p>
+        <div className="bootcamps">
+          <div>
+            <h4>Power Up with Elite Bootcamps.</h4>
+            <p>
+            Unleash your potential with our gym bootcamp, where every session is a step towards a stronger, fitter you. Our expert trainers design intense, varied workouts that target all muscle groups, combining cardio, strength, and functional training.
+            </p>
+          </div>
+          <div>
+            <h4>Elevate Your Fitness Game with Bootcamp.</h4>
+            <p>
+            These high-intensity sessions blend strength training, cardio, and endurance exercises to challenge your body in new ways. Led by experienced trainers, each workout is designed to push your limits, burn calories, and build lean muscle. 
+            </p>
+          </div>
+          <div>
+            <h4>Push Past Your Limits with Our Bootcamp.</h4>
+            <p>
+            Are you ready to break through your fitness barriers? Our Bootcamp is not just a workout—it's a challenge, an adventure, and a path to the best version of yourself. With a mix of intense strength and cardio drills, every session is designed to push you harder and farther than you thought possible.
+            </p>
+          </div>
+          <div>
+            <h4>Get Stronger, Faster, and Fitter.</h4>
+            <p>
+            Whether you're aiming to lose weight, tone up, or simply feel unstoppable, our Bootcamp is your ticket to a fitter, stronger you. Don’t just work out—transform your body and mindset with us!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WorkoutSessions;
